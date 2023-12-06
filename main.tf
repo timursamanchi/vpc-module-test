1394e2449d974378f847626774293d0bdfba3454
timursamanchi <timur.samanchi@gmail.com>
2023-12-06 23:05:03 +0000

    initial commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "aws_availability_zones" "azs" {}
 module "my-test-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
   version         = "5.2.0"
-  name            = "downham-module-vpc"
+  name            = "tf-jenkins-vpc"
   cidr            = "10.0.0.0/16"
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
@@ -19,14 +19,30 @@ module "my-test-vpc" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "module-VPC-ts"
+    Name = "tf-jenkins-vpc"
   }
 
   public_subnet_tags = {
-    Name = "PUB-module-VPC-ts"
+    Name = "tf-jenkins-PUB"
   }
 
   private_subnet_tags = {
-    Name = "PRIV-module-VPC-ts"
+    Name = "tf-jenkins-PRIV"
+  }
+}
+module "aws_module_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  name        = "tf-jenkins-sg"
+  description = "Security group with HTTP, HTTPS, SSH & Jenkins reverse proxy ports open for everybody (IPv4 CIDR), egress ports are all world open"
+  vpc_id      = module.my-test-vpc.vpc_id
+
+  ingress_rules       = ["ssh-tcp", "https-443-tcp", "http-8080-tcp", "http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules        = ["all-all"]
+
+  tags = {
+    Name = "tf-jenkins-sg"
   }
 }
